96294ee52e25010b0f1b8357b70d1ac4cd959cae
stha9840 <aayushstha610@gmail.com>
2025-05-22 18:58:21 +0545

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",            // if you’re using Vite or have an index.html
+    "./src/**/*.{js,ts,jsx,tsx}" // this covers all source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
