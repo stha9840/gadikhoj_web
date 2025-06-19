ae5365518c82e56d18ed91da250e48b6fca199ad
stha9840 <aayushstha610@gmail.com>
2025-06-19 18:05:15 +0545

    Use Admin User(hooks) added

---
+++ src/hooks/admin/useAdminUser.js
@@ -0,0 +1,64 @@
+import { keepPreviousData,useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+
+//useQuery -> Get request state
+import { useState } from "react";
+import { getAllUserService, createUserService, updateUserService, deleteUserService } from "../../services/admin/userService";
+
+export const useAdminUser = (page = 1, limit = 5) => {
+  const query = useQuery({
+    queryKey: ["admin_user", page, limit],
+    queryFn: () => getAllUserService({ page, limit }),
+    keepPreviousData: true,
+  });
+
+  return { ...query };
+};
+  export const useCreateUser = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: ["admin_create_user"],
+    mutationFn: createUserService,
+    onSuccess: () => {
+      queryClient.invalidateQueries("admin_user");
+    }
+  });
+};
+
+export const useUpdateOneUser = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: ({ id, data }) => updateUserService(id, data),
+    mutationKey: ["admin_user_update"],
+    onSuccess: () => {
+      toast.success("Updated")
+      queryClient.invalidateQueries(
+        ["admin_user", "admin_user_details"]
+      )
+    },
+    onError: (err) => {
+      toast.error(err.message || " Update Failed")
+    }
+
+  })
+}
+export const useDeleteOneUser = () =>{
+  const queryClient = useQueryClient()
+  return useMutation(
+    {
+      mutationFn: deleteUserService,
+      mutationKey : ["admin_user_delete"],
+      onSuccess: () =>{
+        toast.success("Delete")
+        queryClient.invalidateQueries(["admin_user"])
+      },
+      onError: (err) =>{
+        toast.error(err.message || "Delete failed")
+      }
+    }
+  )
+}
+
+
+
