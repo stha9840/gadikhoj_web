26a742a6e813627c1c22a3cc3e4075e7e6d91eb9
stha9840 <aayushstha610@gmail.com>
2025-06-20 05:46:25 +0545

    Vehicle Service added

---
+++ src/services/admin/vehicleService.js
@@ -0,0 +1,49 @@
+import { 
+  getAllVehicleApi, createVehicleApi, getOneVehicleApi, 
+  updateOneVehicleApi, deleteOneVehicleApi 
+} from "../../api/admin/vehicleApi";
+
+export const getAllVehicleService = async () => {
+  try {
+    const response = await getAllVehicleApi();
+    return response.data;
+  } catch (err) {
+    throw err.response?.data || { message: 'Failed to fetch vehicles' };
+  }
+};
+
+export const createVehicleService = async (data) => {
+  try {
+    const response = await createVehicleApi(data);
+    return response.data;
+  } catch (err) {
+    throw err.response?.data || { message: 'Failed to create vehicle' };
+  }
+};
+
+export const getOneVehicleService = async (id) => {
+  try {
+    const response = await getOneVehicleApi(id);
+    return response.data;
+  } catch (err) {
+    throw err.response?.data || { message: 'Failed to load vehicle' };
+  }
+};
+
+export const updateVehicleService = async (id, data) => {
+  try {
+    const response = await updateOneVehicleApi(id, data);
+    return response.data;
+  } catch (err) {
+    throw err.response?.data || { message: 'Failed to update vehicle' };
+  }
+};
+
+export const deleteVehicleService = async (id) => {
+  try {
+    const response = await deleteOneVehicleApi(id);
+    return response.data;
+  } catch (err) {
+    throw err.response?.data || { message: 'Failed to delete vehicle' };
+  }
+};
