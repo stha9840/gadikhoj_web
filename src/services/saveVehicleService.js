623e6b0a6b90c7d33bfebe80fffeeb21e03c7e68
stha9840 <aayushstha610@gmail.com>
2025-07-05 12:37:53 +0545

    Save Vehicle Service added

---
+++ src/services/saveVehicleService.js
@@ -0,0 +1,17 @@
+// src/services/savedVehicleService.js
+import axios from "../api/saveVehicleApi";
+
+// 🔹 Get all saved vehicles of current user
+export const getSavedVehiclesService = () => {
+  return axios.get("/saved-vehicles");
+};
+
+// 🔹 Add a vehicle to favorites
+export const addSavedVehicleService = (vehicleId) => {
+  return axios.post("/saved-vehicles", { vehicleId });
+};
+
+// 🔹 Remove a vehicle from favorites
+export const removeSavedVehicleService = (vehicleId) => {
+  return axios.delete(`/saved-vehicles/${vehicleId}`);
+};
